b69070069dd576dfd92440a74b79ae8c5fa1331e
smfcoder <smfcoder17@gmail.com>
2020-05-24 00:03:11 -0300

    About to work on images

---
+++ examples/basic-animation.js
@@ -0,0 +1,94 @@
+import * as Utils from './lib/Utilities.js'
+import Canvas from './lib/Canvas.js'
+import {Random} from './lib/Random.js'
+import {RectShape, CircleShape, Shape, PolygonShape} from './lib/Shapes.js'
+
+let canvas = new Canvas('myCanvas', window.innerWidth - 10, window.innerHeight - 10);
+let entities = [];
+let mouse = new Utils.Vector2();
+
+function init() {
+    document.addEventListener('keydown', (evt = new KeyboardEvent()) => {
+        if (evt.keyCode == Utils.Keys.Escape) {
+            canvas.stopLoop();
+        }
+        else if (evt.keyCode == Utils.Keys.Left) {
+            v.x = -5;
+        }
+        else if (evt.keyCode == Utils.Keys.Right) {
+            v.x = 5;
+        }
+        else if (evt.keyCode == Utils.Keys.Down) {
+            v.y = 5;
+        }
+        else if (evt.keyCode == Utils.Keys.Up) {
+            v.y = -5;
+        }
+    });
+
+    canvas.c.addEventListener('mousemove', (ev) => {
+        mouse.x = ev.x;
+        mouse.y = ev.y;
+    });
+
+    console.log(Shape.Fill | Shape.Stroke);
+
+    for (let i = 0; i < 1200; i++) {
+        let radius = Random.int(1, 2);
+        //let color = Utils.Color.rgba(Random.int(0, 50), Random.int(100, 255), Random.int(150, 255), Random.float(1, 1));
+        let color = Random.element([Utils.Color.Red, Utils.Color.Dark, Utils.Color.Blue, Utils.Color.LightBlue]);
+        //let bColor = Utils.Color.rgba(Random.int(50, 200), Random.int(0, 200), Random.int(50, 200), Random.float(0.5, 1));
+        let pos = new Utils.Vector2(Random.int(radius, canvas.width - radius), Random.int(radius, canvas.height - radius));
+        let entity =  {
+            r: radius,
+            obj: new CircleShape(pos.x, pos.y, radius, Random.element([ Shape.Fill | Shape.Stroke])),
+            v: new Utils.Vector2(Random.element([-1, 1]), Random.element([-1, 1]))
+        };
+        entity.obj.setColor(color);
+        //entity.obj.thickness = Random.int(1, 3);
+        //entity.obj.setBorderColor(bColor);
+        entities.push(entity);
+    }
+    console.log(entities);
+}
+
+function update(dt = new Utils.DeltaTime()) {
+    let maxRadius = 40, range = 70;
+    entities.forEach(entity => {
+        if (entity.obj.x <= 0 || entity.obj.x >= canvas.width) {
+            entity.v.x *= -1;
+        }
+        if (entity.obj.y <= 0 || entity.obj.y >= canvas.height) {
+            entity.v.y *= -1;
+        }
+        entity.obj.move(entity.v.x, entity.v.y);
+
+        // effect
+        if (mouse != undefined)
+        {
+            if (entity.obj.x >= mouse.x - range && entity.obj.x <= mouse.x + range
+                && entity.obj.y >= mouse.y - range && entity.obj.y <= mouse.y + range) 
+            {
+                let r = (entity.obj.radius < maxRadius) ? (entity.obj.radius + (dt.sec * range)) : maxRadius;
+                entity.obj.setRadius(r);
+            }
+            else if (entity.obj.radius != entity.r){
+                let r = (entity.obj.radius > entity.r) ? Math.abs(entity.obj.radius - (dt.sec * maxRadius)) : entity.r;
+                entity.obj.setRadius(r);
+            }
+
+        }
+    });
+}
+
+function draw() {
+    canvas.clear(Utils.Color.LightGray);
+    entities.forEach(entity => {
+        entity.obj.draw(canvas.ctx);
+    });
+}
+
+canvas.onInit = init;
+canvas.onUpdate = update;
+canvas.onDraw = draw;
+canvas.mainLoop(60);
--- main.js
+++ main.js
@@ -1,51 +1,56 @@
-import * as Utils from './Utilities.js'
-import {Canvas} from './Canvas.js'
-import {RectShape, CircleShape, Shape} from './Shapes.js'
+import Canvas from "./lib/Canvas.js";
+import { DeltaTime, Color, Direction, Keys, Vector2 } from "./lib/Utilities.js";
+import { RectShape, Shape, CircleShape } from "./lib/Shapes.js";
+import Random from "./lib/Random.js"
 
-let canvas = new Canvas('myCanvas', window.innerWidth - 10, window.innerHeight - 10);
+let canvas = new Canvas('myCanvas', innerWidth - 5, innerHeight - 5);
+let mouse = {x: 0, y: 0};
+function Particle(velocity = new Vector2(0, 0), circle = new CircleShape(0, 0, 5, Shape.Fill)){ 
+    this.velocity = velocity;
+    this.body = circle;
+    this.radians = 0;
 
-let v = {x: 0, y: 0};
-let circle = new CircleShape(100, 100, 15, Shape.Fill);
-circle.setColor(Utils.Color.rgb(230, 230, 50));
-circle.setShadow(Utils.Shadow(2, 2, 5, Utils.Color.Teal));
+    this.update = (dt) => {
+        this.radians += this.velocity * dt.sec;
+        this.body.x = this.body.x + Math.cos(this.radians) * 5;
+        this.body.y = this.body.y + Math.sin(this.radians) * 5;
+    };
 
-let rect2 = new RectShape(1, 1, 300, 200, Shape.Fill);
-rect2.setColor(Utils.Color.LGradient(Utils.Direction.Right, 0, Utils.Color.Blue, 1, Utils.Color.Purple));
-rect2.setOrigin(rect2.width / 2, rect2.height / 2);
-rect2.setRotation(0);
+    this.draw = function() { this.body.draw(canvas.ctx); };
+};
 
-canvas.onInit = () => {
-    document.addEventListener('keydown', (evt = new KeyboardEvent()) => {
-        v = {x: 0, y: 0};
-        if (evt.keyCode == Utils.Keys.Escape) {
-            canvas.stopLoop();
-        }
-        else if (evt.keyCode == Utils.Keys.Left) {
-            v.x = -5;
-        }
-        else if (evt.keyCode == Utils.Keys.Right) {
-            v.x = 5;
-        }
-        else if (evt.keyCode == Utils.Keys.Down) {
-            v.y = 5;
-        }
-        else if (evt.keyCode == Utils.Keys.Up) {
-            v.y = -5;
-        }
+let particles = [];
+function init() {
+    // var initialisation
+    for (let i = 0; i < 15; i++) {
+        particles[i] = new Particle(Random.int(2, 4), new CircleShape(Random.int(200, 225), Random.int(200, 215), 5, Shape.Fill));
+        particles[i].body.setColor(Random.element([Color.Blue, Color.Cyan, Color.Purple]));
+    }
+
+    // eventListener initialisation
+    canvas.c.addEventListener('mousemove', onMouseMove);
+
+    // functions initialisation
+    function onMouseMove(evt = new MouseEvent()) {
+        mouse.x = evt.clientX;
+        mouse.y = evt.clientY;
+    }
+}
+
+function update(dt = new DeltaTime()) {
+    particles.forEach((particle) => {
+        particle.update(dt);
     });
-};
+}
 
-let r = 0;
-canvas.onUpdate = (dt) => {
-    circle.move(v.x, v.y);
-    r++;
-    //rect2.setRotation(r);
-};
+function draw() {
+    canvas.clear(Color.LightGray);
+    particles.forEach((particle) => {
+        particle.draw();
+    });
+}
 
-canvas.onDraw = () => {
-    canvas.clear();
-    rect2.draw(canvas.ctx);
-    circle.draw(canvas.ctx);
-};
-
+canvas.onInit = init;
+canvas.onUpdate = update;
+canvas.onDraw = draw;
 canvas.mainLoop(60);
